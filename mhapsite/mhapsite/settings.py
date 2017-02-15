68ed53b5f2b385c9574b524a3ce698e38323c3f7
Ayush Kohli <ayushk96@hotmail.com>
2017-02-14 18:52:43 -0600

    Generated new secret key and added to env variable

--- mhapsite/mhapsite/settings.py
+++ mhapsite/mhapsite/settings.py
@@ -20,8 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+dz=x2gw(8@$^&$d9al6%ja)!r5bt58l&4uu#zhyqk47wdfutp'
+SECRET_KEY = os.environ.get('SECRET')
 
+#print SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
