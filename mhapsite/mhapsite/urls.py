aa8738ec4da87b0b64cdb3bd02a80107096e526d
Ayush Kohli <ayushk96@hotmail.com>
2017-02-19 15:28:59 -0600

    Got login and logout working. Need email registration and password reset

--- mhapsite/mhapsite/urls.py
+++ mhapsite/mhapsite/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^mhap/', include('mhap.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout,  {'next_page': '/mhap/'}, name='logout'), #point to mhap
+    #{'template_name': 'logout.html'}
 ]
