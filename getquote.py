82f8febd4d227f5ffc0f30e728b4675c042eb7cf
Ayush Kohli <akohli96@users.noreply.github.com>
2017-04-07 23:03:16 -0500

    Deleting quote API GET request script

--- getquote.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-API="http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json"
-
-quote_json=requests.get(API).json()
-
-print quote_json['quoteText']
-print quote_json['quoteAuthor']
-
