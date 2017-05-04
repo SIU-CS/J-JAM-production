1f91e0603cf8cbb1872c5e7c927c44b3e899f118
markiragoldberg@outlook.com <markiragoldberg@outlook.com>
2017-05-04 17:42:58 -0500

    added some tests

--- mhapsite/mhap/tests.py
+++ mhapsite/mhap/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase,Client
 from django.urls import reverse
 from mhap.models import Quote
+from mhap.models import Profile
 # Create your tests here.
 
 class AppViewTest(TestCase):
@@ -12,6 +13,18 @@ class AppViewTest(TestCase):
     def test_create_page_redirect(self):
         response = self.client.get(reverse('mhap:create'))
         self.assertEqual(response.url,'/login/?next=/mhap/create/')
+        
+    def test_create_page_redirect(self):
+        response = self.client.get(reverse('mhap:list/'))
+        self.assertEqual(response.url,'/login/?next=/mhap/list/')
+        
+    def test_create_page_redirect(self):
+        response = self.client.get(reverse('mhap:bot'))
+        self.assertEqual(response.url,'/login/?next=/mhap/bot/')
+        
+    def test_create_page_redirect(self):
+        response = self.client.get(reverse('mhap:settings'))
+        self.assertEqual(response.url,'/login/?next=/mhap/settings/')
 
 class QuoteTest(TestCase):
     def setUp(self):
@@ -21,3 +34,8 @@ class QuoteTest(TestCase):
         default_quote = Quote.objects.get(quote="quote")
         self.assertEqual(default_quote.quote,"quote")
         self.assertEqual(default_quote.author,"author")
+        
+class ProfileTest(TestCase):
+    def setUp(self):
+        Profile.objects.create(
+    
