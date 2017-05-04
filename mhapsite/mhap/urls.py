3458f40bb942fa181f8e306b1533fe4c68e3b5e9
markiragoldberg@outlook.com <markiragoldberg@outlook.com>
2017-05-04 18:14:23 -0500

    add name to settings page

--- mhapsite/mhap/urls.py
+++ mhapsite/mhap/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     url(r'^list/(?P<slug>[\w-]+)/$', views.post_detail, name='detail'),
     url(r'^list/(?P<slug>[\w-]+)/edit/$', views.post_update, name='update'),
     url(r'^list/(?P<slug>[\w-]+)/delete/$', views.post_delete),
-    url(r'^settings/$', views.settings),
+    url(r'^settings/$', views.settings, name="settings"),
     url(r'^settings/password/$', views.change_password, name='change_password'),
     url(r'^bot/$', views.bot_page, name='bot_page'),
 ]
